85e2fb6472586985845f6970142889deec5df46a
rahulT-17 <rahulthk1702@gmail.com>
2026-05-13 09:05:38 +0530

    feat(settings): configured rag setting

--- app/core/settings.py
+++ app/core/settings.py
@@ -10,5 +10,11 @@ class Settings(BaseSettings):
     env: str = "dev"
     database_url: str
     log_level: str = "INFO"
+    rag_docs_dir: str = "data/docs"
+    chroma_persist_dir: str = "data/chroma"
+    chroma_collection_name: str = "pgagi_rag"
+    embedding_model_name: str = "sentence-transformers/all-MiniLM-L6-v2"
+    chunk_size: int = 80
+    chunk_overlap: int = 20
 
 settings = Settings()
